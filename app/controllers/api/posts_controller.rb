dcd6ab1a27f464d617d991d0977349df130034e0
Micah Sterling <msterling89@gmail.com>
2021-04-01 21:25:40 -0500

    add index action

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,6 @@
 class Api::PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    render 'index.json.jb'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# post = Post.new(title: "How to clean your stove", content: "clean using some degreaser and a soft towel")
+# post.save
+# post = Post.new(title: "How to clean your floors", content: "best practice is to clean with a fresh towel")
+# post.save
+# post = Post.new(title: "How to clean your bathtub", content: "clean using some tide and a soft bristled brush")
+# post.save
+
+p "complete"
