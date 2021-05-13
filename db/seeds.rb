35a9c9edca7c13a29a7991f20cb04a302db9495b
Micah Sterling <msterling89@gmail.com>
2021-05-12 20:33:51 -0500

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,116 +6,116 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# post = Post.new(title: "How to clean your stove", content: "clean using some degreaser and a soft towel")
-# post.save
-# post = Post.new(title: "How to clean your floors", content: "best practice is to clean with a fresh towel")
-# post.save
-# post = Post.new(title: "How to clean your bathtub", content: "clean using some tide and a soft bristled brush")
-# post.save
+post = Post.new(title: "How to clean your stove", content: "clean using some degreaser and a soft towel")
+post.save
+post = Post.new(title: "How to clean your floors", content: "best practice is to clean with a fresh towel")
+post.save
+post = Post.new(title: "How to clean your bathtub", content: "clean using some tide and a soft bristled brush")
+post.save
 
-# categoy = Category.new(category: "Kitchen",)
-# categoy.save
-# categoy = Category.new(category: "Bedroom",)
-# categoy.save
-# categoy = Category.new(category: "Bathroom",)
-# categoy.save
-# categoy = Category.new(category: "Living Room",)
-# categoy.save
-# categoy = Category.new(category: "Laundry",)
-# categoy.save
+categoy = Category.new(category: "Kitchen",)
+categoy.save
+categoy = Category.new(category: "Bedroom",)
+categoy.save
+categoy = Category.new(category: "Bathroom",)
+categoy.save
+categoy = Category.new(category: "Living Room",)
+categoy.save
+categoy = Category.new(category: "Laundry",)
+categoy.save
 
-# tag = Tag.new(name: "product")
-# tag.save
-# tag = Tag.new(name: "microwave")
-# tag.save
-# tag = Tag.new(name: "sink")
-# tag.save
-# tag = Tag.new(name: "toilet")
-# tag.save
-# tag = Tag.new(name: "dishes")
-# tag.save
-# tag = Tag.new(name: "stove")
-# tag.save
-# tag = Tag.new(name: "oven")
-# tag.save
-# tag = Tag.new(name: "shower")
-# tag.save
-# tag = Tag.new(name: "bathtub")
-# tag.save
-# tag = Tag.new(name: "fan")
-# tag.save
-# tag = Tag.new(name: "vent")
-# tag.save
-# tag = Tag.new(name: "grout")
-# tag.save
-# tag = Tag.new(name: "carpet")
-# tag.save
-# tag = Tag.new(name: "mold")
-# tag.save
-# tag = Tag.new(name: "rust")
-# tag.save
-# tag = Tag.new(name: "burnt")
-# tag.save
-# tag = Tag.new(name: "stain")
-# tag.save
-# tag = Tag.new(name: "washer")
-# tag.save
-# tag = Tag.new(name: "dryer")
-# tag.save
-# tag = Tag.new(name: "mop")
-# tag.save
-# tag = Tag.new(name: "vacuum")
-# tag.save
-# tag = Tag.new(name: "wood")
-# tag.save
-# tag = Tag.new(name: "sponge")
-# tag.save
-# tag = Tag.new(name: "bleach")
-# tag.save
-# tag = Tag.new(name: "dust")
-# tag.save
-# tag = Tag.new(name: "upholstery")
-# tag.save
-# tag = Tag.new(name: "rug")
-# tag.save
-# tag = Tag.new(name: "closet")
-# tag.save
-# tag = Tag.new(name: "pet")
-# tag.save
-# tag = Tag.new(name: "pet hair")
-# tag.save
-# tag = Tag.new(name: "suede")
-# tag.save
-# tag = Tag.new(name: "leather")
-# tag.save
-# tag = Tag.new(name: "stainless steel")
-# tag.save
-# tag = Tag.new(name: "non-stick")
-# tag.save
-# tag = Tag.new(name: "curtain")
-# tag.save
-# tag = Tag.new(name: "blinds")
-# tag.save
-# tag = Tag.new(name: "microfiber")
-# tag.save
-# tag = Tag.new(name: "wool")
-# tag.save
-# tag = Tag.new(name: "screen")
-# tag.save
-# tag = Tag.new(name: "refridgerator")
-# tag.save
-# tag = Tag.new(name: "freezer")
-# tag.save
-# tag = Tag.new(name: "floor")
-# tag.save
-# tag = Tag.new(name: "scuffs")
-# tag.save
+tag = Tag.new(name: "product")
+tag.save
+tag = Tag.new(name: "microwave")
+tag.save
+tag = Tag.new(name: "sink")
+tag.save
+tag = Tag.new(name: "toilet")
+tag.save
+tag = Tag.new(name: "dishes")
+tag.save
+tag = Tag.new(name: "stove")
+tag.save
+tag = Tag.new(name: "oven")
+tag.save
+tag = Tag.new(name: "shower")
+tag.save
+tag = Tag.new(name: "bathtub")
+tag.save
+tag = Tag.new(name: "fan")
+tag.save
+tag = Tag.new(name: "vent")
+tag.save
+tag = Tag.new(name: "grout")
+tag.save
+tag = Tag.new(name: "carpet")
+tag.save
+tag = Tag.new(name: "mold")
+tag.save
+tag = Tag.new(name: "rust")
+tag.save
+tag = Tag.new(name: "burnt")
+tag.save
+tag = Tag.new(name: "stain")
+tag.save
+tag = Tag.new(name: "washer")
+tag.save
+tag = Tag.new(name: "dryer")
+tag.save
+tag = Tag.new(name: "mop")
+tag.save
+tag = Tag.new(name: "vacuum")
+tag.save
+tag = Tag.new(name: "wood")
+tag.save
+tag = Tag.new(name: "sponge")
+tag.save
+tag = Tag.new(name: "bleach")
+tag.save
+tag = Tag.new(name: "dust")
+tag.save
+tag = Tag.new(name: "upholstery")
+tag.save
+tag = Tag.new(name: "rug")
+tag.save
+tag = Tag.new(name: "closet")
+tag.save
+tag = Tag.new(name: "pet")
+tag.save
+tag = Tag.new(name: "pet hair")
+tag.save
+tag = Tag.new(name: "suede")
+tag.save
+tag = Tag.new(name: "leather")
+tag.save
+tag = Tag.new(name: "stainless steel")
+tag.save
+tag = Tag.new(name: "non-stick")
+tag.save
+tag = Tag.new(name: "curtain")
+tag.save
+tag = Tag.new(name: "blinds")
+tag.save
+tag = Tag.new(name: "microfiber")
+tag.save
+tag = Tag.new(name: "wool")
+tag.save
+tag = Tag.new(name: "screen")
+tag.save
+tag = Tag.new(name: "refridgerator")
+tag.save  
+tag = Tag.new(name: "freezer")
+tag.save
+tag = Tag.new(name: "floor")
+tag.save
+tag = Tag.new(name: "scuffs")
+tag.save
 
-# Product.new(name:"kosher salt", post_id: 12, image: "https://i5.walmartimages.com/asr/5df4b8a2-fcda-400a-abc3-7fd6a447f189_2c8cb38a8cb329535a3d255f66b856e67.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff")
-# product.save
-# product = Product.new(name: "Mr Clean MagicEraser", post_id: 24, image: "https://images.albertsons-media.com/is/image/ABS/960505854$ecom-pdp-desktop$&defaultImage=Not_Available&defaultImage=Not_Available")
-# product.save
-# product = Product.new(name: "Bar Keepers Friend", post_id: 26, image: "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:ecoc_fit,w_730,h_749/k%2Farchive%2Fed99de31f3f3353f64f331fe8f3a77d69ab513b6")
-#   product.save
+Product.new(name:"kosher salt", post_id: 12, image: "https://i5.walmartimages.com/asr/5df4b8a2-fcda-400a-abc3-7fd6a447f189_2c8cb38a8cb329535a3d255f66b856e67.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff")
+product.save
+product = Product.new(name: "Mr Clean MagicEraser", post_id: 24, image: "https://images.albertsons-media.com/is/image/ABS/960505854$ecom-pdp-desktop$&defaultImage=Not_Available&defaultImage=Not_Available")
+product.save
+product = Product.new(name: "Bar Keepers Friend", post_id: 26, image: "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:ecoc_fit,w_730,h_749/k%2Farchive%2Fed99de31f3f3353f64f331fe8f3a77d69ab513b6")
+  product.save
 
 p "complete"
